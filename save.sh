c8a09c9e3260eb3b5524ca870ea372829f932a9f
sstark21 <va.sabitov@gmail.com>
2019-04-29 18:49:40 +0300

    1% ready

--- save.sh
+++ save.sh
@@ -1,1 +1,1 @@
-git add libft/* libft.a main.c convert_to_char.c ft_ls.h konspekt_ls.c save.sh && git commit -m "1% ready" && git push
+git add libft/* libft.a main.c convert_to_char.c ft_ls.h konspekt_ls.c check_flags.c cr_struct.c save.sh && git commit -m "1% ready" && git push origin master
